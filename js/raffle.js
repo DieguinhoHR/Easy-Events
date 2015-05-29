46d8c34d40056b4b272949490224352e74ce3dfc
eduardoafontana <fontanadnb@gmail.com>
2015-05-28 21:04:14 -0300

    Adicionado TDD do sorteio.

---
+++ js/raffle.js
@@ -0,0 +1,16 @@
+﻿function executeRaffle() {	
+
+	var participant = {"raffled":null, "message":"Participantes válidos insuficientes!"};
+
+	var allChekeds = $.grep(allParticipants, function(n, i) {
+		return n.checked === "1";
+	});
+
+	if(typeof allChekeds === "undefined")
+		return participant;
+	
+	if(allChekeds.length > 1 )
+		return { "raffled":allChekeds[Math.floor(Math.random()*allChekeds.length)], "message":"" };
+	
+	return participant;
+}
